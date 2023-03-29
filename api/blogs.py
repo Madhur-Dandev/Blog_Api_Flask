8c6ccae9671aa8aa8e5def1d601dde6fdfc17179
Madhur-Dandev <pradip95273@gmail.com>
2023-03-29 16:33:00 +0530

    Edit Blog endpoint and image preview done

--- api/blogs.py
+++ api/blogs.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request as req, make_response as res, render_template as render
+from flask import Blueprint, jsonify, request as req, make_response as res, send_from_directory
 from database import db
 from sqlalchemy import text, exc
 from exception import UserDefined
@@ -8,12 +8,14 @@ from middleware import check_token
 from string import ascii_lowercase, ascii_uppercase, digits
 from random import choice
 from os import path, mkdir
-from shutil import rmtree
+from shutil import rmtree, move
+
+load_dotenv()
 
 blogs = Blueprint("blogs", __name__, url_prefix="/blogs")
 
 def genFolderName():
-    return "".join([choice(f"{ascii_lowercase}{ascii_uppercase}{dir}") for i in range(8)])    
+    return "".join([choice(f"{ascii_lowercase}{ascii_uppercase}{digits}") for i in range(8)])    
 
 @blogs.get("/")
 @blogs.get("/<sort>")
@@ -57,7 +59,7 @@ def create(token, resp):
                     raise UserDefined({"message": "Length of title must be 500 or less."})    
             
         else:
-            raise UserDefined({"message": "Please log in first."})
+            return res(jsonify({"message": "Please log in first."}), 401)
     except (Exception, IOError) as e:
         if isinstance(e, IOError):
             print(e)
@@ -177,9 +179,12 @@ def delete(token, resp, id):
                             print(e)
                             return res(jsonify({"message": "Server Error. Please try again."}), 500)
 
-                    conn.execute(text(f'''DELETE new_blogs, user_blog_stats, blog_comments, blog_comment_replies FROM new_blogs LEFT JOIN user_blog_stats ON new_blogs.id = user_blog_stats.blog_id LEFT JOIN blog_comments ON new_blogs.id = blog_comments.blog_id LEFT JOIN blog_comment_replies ON new_blogs.id = blog_comment_replies.blog_id WHERE new_blogs.id = {id}'''))
+                    delete_res = conn.execute(text(f'''DELETE new_blogs, user_blog_stats, blog_comments, blog_comment_replies FROM new_blogs LEFT JOIN user_blog_stats ON new_blogs.id = user_blog_stats.blog_id LEFT JOIN blog_comments ON new_blogs.id = blog_comments.blog_id LEFT JOIN blog_comment_replies ON new_blogs.id = blog_comment_replies.blog_id WHERE new_blogs.id = {id} AND new_blogs.user_id = {resp.get("id")}''')).rowcount
 
-                    return res(jsonify({"message": "Blog Deleted!"}))
+                    if delete_res:
+                        return res(jsonify({"message": "Blog Deleted!"}), 200)
+                    else:
+                        raise UserDefined({"message": "Unable to Delete"})
                 else:
                     raise UserDefined({"message": "Sorry Blog doesn't exists."})
         except (Exception, exc.SQLAlchemyError) as e:
@@ -191,3 +196,73 @@ def delete(token, resp, id):
             return res(jsonify({"message": "Server Error"}), 500)
     else:
         return res(jsonify({"message": "Please Login First"}), 401)
+    
+
+@blogs.put("/update/<string:token>/<int:blog_id>")
+@check_token
+def upadateBlog(token, resp, blog_id):
+    if resp.get("loggedin"):
+        if req.form:
+            try:
+                with db.connect() as conn:
+                    blog_exists = conn.execute(text(f'''SELECT * FROM new_blogs WHERE id = {blog_id}''')).mappings().first()
+                    if blog_exists:
+                        if blog_exists.get("user_id") == resp.get("id"):
+                            title = req.form.get("title")
+                            description = req.form.get("description")
+                            blog_img = req.files.get("blog_img")
+
+                            # if title or description or blog_img:
+                            query_str = ""
+                            blog_img_str = ""
+                            if blog_img:
+                                if "image" in blog_img.mimetype:
+                                    folderName = blog_exists.get("blog_image").rsplit("/", 2)[-2] if blog_exists.get("blog_image") else genFolderName()
+                                    if not path.exists(path.join(getenv("UPLOAD_FOLDER"), folderName)):
+                                        mkdir(path.join(getenv("UPLOAD_FOLDER"), folderName))
+
+                                    try:
+                                        blog_img.save(path.join(getenv("UPLOAD_FOLDER"), folderName, f"blog_img.{blog_img.filename.rsplit('.', 1)[-1]}"))
+                                        blog_img_str = f'''http://localhost:5000/api/blogs/img/{folderName}/blog_img.{blog_img.filename.rsplit('.', 1)[-1]}'''
+                                    except (IOError, OSError) as e:
+                                        print(e)
+                                        print(e.args[0])
+                                        return res(jsonify({"message": "Server Error"}), 500)
+                                    
+                                    
+                                    query_str = f'''UPDATE new_blogs SET blog_title = {blog_exists.get("blog_title") if blog_exists.get("blog_title") else title}, blog_description =  {blog_exists.get("blog_description") if blog_exists.get("blog_description") else description}, blog_image = {blog_img_str},'''
+                                else:
+                                    raise UserDefined({"message": "Uploaded file must be image."})
+                            query_str = f'''UPDATE new_blogs SET blog_title = "{blog_exists.get("blog_title") if blog_exists.get("blog_title") == title else title}", blog_description =  "{blog_exists.get("blog_description") if blog_exists.get("blog_description") == description else description}", blog_image = "{blog_exists.get("blog_image") if blog_img_str == "" else blog_img_str}" WHERE id = {blog_id} AND user_id = {resp.get("id")}'''
+
+                            update_resp = conn.execute(text(query_str)).rowcount
+
+                            if not update_resp:
+                                return res(jsonify({"message": "Something went wrong. Try again later!"}), 500)
+
+                            return res(jsonify({"message": "Blog Updated!"}), 200)
+                            # else:
+                            #     return res(jsonify({"message": "Nothing to change!"}), 200)
+                        else:
+                            return res(jsonify({"message": "Not allowed!"}), 401)
+                    else:
+                        raise UserDefined({"message": "Blog does not exists!"})
+            except (Exception, exc.SQLAlchemyError) as e:
+                if isinstance(e, UserDefined):
+                    print(e.args[0])
+                    return res(jsonify(e.args[0]), 400)
+
+                print(e)
+                return res(jsonify({"message": "Server Error"}), 500)
+        else:
+            return res(jsonify({"message": "Data is required."}), 400)
+    else:
+        return res(jsonify({"message": "Please Log In First"}), 401)
+    
+@blogs.get("/img/<string:folderName>/<string:fileName>")
+def blog_img(folderName, fileName):
+    try:
+        return send_from_directory(path.join(getenv("UPLOAD_FOLDER"), folderName), fileName, as_attachment=False)
+    except (FileNotFoundError, FileExistsError, OSError) as e:
+        print(e)
+        return res(jsonify({"message": "File Does not Exist!"}))
